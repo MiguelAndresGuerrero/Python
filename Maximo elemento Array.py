6ba0d18fe2483c9aa8583830196bb607da31407e
Andres Guerrero <166781613+MiguelAndresGuerrero@users.noreply.github.com>
2024-07-31 08:03:21 -0500

    Add files via upload

---
+++ Maximo elemento Array.py
@@ -0,0 +1,5 @@
+array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+maximo = max(array)
+
+print("El valor máximo en el array es:", maximo)
---
+++ Revertir una cadena.py
@@ -0,0 +1,8 @@
+cadena = "Hola Mundo"
+
+cadena_invertida = ""
+
+for caracter in cadena:
+    cadena_invertida = caracter + cadena_invertida
+
+print("La cadena invertida es: ", cadena_invertida)
